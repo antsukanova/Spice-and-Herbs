f6aefd40465f3c574b84d20b38bb8cefd78aeccd
Annabell <Ts.Nyutka@gmail.com>
2019-01-17 21:46:27 +0200

    Add basic structure for C# Blazor app

---
+++ SpiceApplication.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using SpiceApplication.App;
+
+namespace SpiceApplication.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
