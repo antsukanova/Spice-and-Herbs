90af4096d803efe2e434023dfacc6fb4d8fd20ab
Anna Tsukanova <38460776+AnnabellFlem@users.noreply.github.com>
2019-04-21 19:30:48 +0300

    Fixed problem with img
    Fixed problem with loading images in map Component and change symbols in text.

--- SpiceApplication.App/wwwroot/js/react/components/mapComponent.js
+++ SpiceApplication.App/wwwroot/js/react/components/mapComponent.js
@@ -30,12 +30,12 @@ class MapComponent extends React.Component {
                 <h3>About</h3>
                 <div id="map">
                     <ul className="continents">
-                        <li className="c1"><a href="#africa">Africa</a></li>
-                        <li className="c2"><a href="#asia">Asia</a></li>
-                        <li className="c3"><a href="#australia">Australia</a></li>
-                        <li className="c4"><a href="#europe">Europe</a></li>
-                        <li className="c5"><a href="#north-america">North America</a></li>
-                        <li className="c6"><a href="#south-america">South America</a></li>
+                        <li className="c1"><a href="#africa"><img src="img/map_africa.jpg" title="Africa" alt="Africa"/></a></li>
+                        <li className="c2"><a href="#asia"><img src="img/map_asia.jpg" title="Asia" alt="Asia"/></a></li>
+                        <li className="c3"><a href="#australia"><img src="img/map_australia.jpg" title="Australia" alt="Australia"/></a></li>
+                        <li className="c4"><a href="#europe"><img src="img/map_europe.jpg" title="Europe" alt="Europe"/></a></li>
+                        <li className="c5"><a href="#north-america"><img src="img/map_namerica.jpg" title="North America" alt="North America"/></a></li>
+                        <li className="c6"><a href="#south-america"><img src="img/map_samerica.jpg" title="South America" alt="South America"/></a></li>
                     </ul>
                 </div>
                 <div id="about-text">
@@ -45,7 +45,7 @@ class MapComponent extends React.Component {
                         sources of Spice, and the rise and fall of empires.
                     </p>
                     <p>
-                        Columbus headed westward from Europe in 1492 to find a sea route to the so-called, &lsquo;˜Land of Spices&lsquo;,
+                        Columbus headed westward from Europe in 1492 to find a sea route to the so-called, &lsquo;ËœLand of Spices&lsquo;,
                         instead he found the New World. Eight years later Vasco da Gama sailed round Africa touching Kozhikode
                         on the South West coast of India. Long before that, Arabs were trading with the then known &sbquo;Orient &rsquo;
                         through land routes, and during the 13th century Marco Polo experienced the attraction of spices in his
